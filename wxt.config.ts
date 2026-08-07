0b0a548b0d6a71cbe752c6cbc259bd84093758b9
Kelvin Almonte <kelvin.almonte95@gmail.com>
2026-08-07 12:51:23 -0400

    feat: add toggle configs & yaml for environments

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'wxt';
 import tailwindcss from '@tailwindcss/vite';
+import yaml from '@modyfi/vite-plugin-yaml';
 
 export default defineConfig({
   srcDir: 'src',
@@ -8,8 +9,9 @@ export default defineConfig({
     description: "New Day. God's Plan. Better You.",
     version: '1.0.0',
     permissions: ['storage', 'alarms', 'notifications'],
+    host_permissions: ['http://localhost:3000/*'],
   },
   vite: () => ({
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), yaml()],
   }),
 });
